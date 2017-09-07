f2474ae7fa645aac8149373d9db5214f72fcdb6a
Saul Artiga <slbrtg@gmail.com>
2017-09-07 14:36:21 -0700

    create project, work on home component style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './root/app.component';
+import { HomeComponent } from './home/home.component';
+import { WorkHistoryComponent } from './work-history/work-history.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorkHistoryComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
